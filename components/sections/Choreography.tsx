c436597199fe12fad07ec391c609c61b0c624139
ItaloSol <italo.sol100@gmail.com>
2025-04-03 15:25:30 -0300

    Video

--- components/sections/Choreography.tsx
+++ components/sections/Choreography.tsx
@@ -34,7 +34,7 @@ export default function Choreography() {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/JetAv3dm5Vo"
+                src="https://www.youtube.com/embed/enMVYygvbwM"
                 title="Coreografia Exclusiva - Luanda Heels"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
